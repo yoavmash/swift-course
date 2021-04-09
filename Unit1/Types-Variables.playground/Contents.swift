aa39da8893614ec7e3b58daf655a78da07e3b8de
yoav mashraki <yoavmashraki@walla.com>
2021-04-09 22:21:49 +0300

    update playground

--- Unit1/Types-Variables.playground/Contents.swift
+++ Unit1/Types-Variables.playground/Contents.swift
@@ -1,5 +1,14 @@
 import UIKit
 
 // Swift
+/*
+ Swift was introduced in 2014
+ - swift is type safe language meaning you can't mix a
+   declard data type with another e.g if var name = "Yoav"
+ 
+   you cannot update name = 10 // won't compile type String
+     is not an Int
+ - modern language
+*/
 
 var str = "Hello, playground"
